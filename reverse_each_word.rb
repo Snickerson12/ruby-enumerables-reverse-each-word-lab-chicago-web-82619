b58fb613851b7d6ea2acc695382f13f8c334b4e7
Laura Nickerson <laura.nickerson@gmail.com>
2019-08-15 22:42:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,3 @@ def reverse_each_word = "Hello there, and how are you?"
   reverse_each_word.each do |reverse_each_word|
     puts "#{reverse_each_word}"
 end
-
-reverse_each_word(string) {|string| "#{string.reverse}"}
